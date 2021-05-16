7ef9d4b1e9b10b303567bd8d2c17545bbdc819dc
Masashi SAKAI <masashi.sakai1986@gmail.com>
2021-05-16 20:12:25 +0900

    バリデーションを追加

--- configuration-properties-demo/src/main/java/com/github/massakai/snippets/configurationpropertiesdemo/config/APIConfig.java
+++ configuration-properties-demo/src/main/java/com/github/massakai/snippets/configurationpropertiesdemo/config/APIConfig.java
@@ -2,10 +2,14 @@ package com.github.massakai.snippets.configurationpropertiesdemo.config;
 
 import java.net.URL;
 import java.time.Duration;
+import javax.validation.constraints.NotNull;
 
 public class APIConfig {
+  @NotNull
   private final URL url;
+  @NotNull
   private final Duration connectionTimeout;
+  @NotNull
   private final Duration readTimeout;
 
   public APIConfig(final URL url, final Duration connectionTimeout, final Duration readTimeout) {
--- configuration-properties-demo/src/main/java/com/github/massakai/snippets/configurationpropertiesdemo/config/DemoProperties.java
+++ configuration-properties-demo/src/main/java/com/github/massakai/snippets/configurationpropertiesdemo/config/DemoProperties.java
@@ -1,11 +1,15 @@
 package com.github.massakai.snippets.configurationpropertiesdemo.config;
 
+import javax.validation.Valid;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties("demo")
 @ConstructorBinding
+@Validated
 public class DemoProperties {
+  @Valid
   private final APIConfig apiConfig;
 
   public DemoProperties(final APIConfig apiConfig) {
